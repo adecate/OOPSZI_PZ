d6ad9b768cfa45c52835b1450df6ef4557c4e04a
adecate <45331133+adecate@users.noreply.github.com>
2021-05-12 08:18:54 +0300

    Add files via upload

---
+++ Models/ParkingAreasdbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web_Service.Models;
+
+namespace Client_WPF.Models
+{
+    public partial class ParkingAreasdbContext : DbContext
+    {
+        public ParkingAreasdbContext()
+        {
+        }
+
+        public ParkingAreasdbContext(DbContextOptions<ParkingAreasdbContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<ParkingAreaInfo> ParkingAreas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var current_dir = new System.IO.DirectoryInfo(System.IO.Directory.GetCurrentDirectory());
+                var application_dir = current_dir.Parent.Parent.Parent.Parent.FullName;
+                string db_path = "Data Source=" + application_dir + "\\Web Service" + "\\ParkingAreas.db";
+
+                optionsBuilder.UseSqlite(db_path);
+            }
+        }
+
+    }
+}
